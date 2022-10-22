1fc13de0052c1479fdf7e4dbe22edf7933f74a3d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-10-22 13:36:39 +0900

    不要な処理を削除

--- Assets/Scripts/Define.cs
+++ Assets/Scripts/Define.cs
@@ -15,45 +15,7 @@ namespace Cookie
         Attack = 1,
         Recovery = 2,
     }
-
-    public enum PassiveSkillType
-    {
-        /// <summary>
-        /// 物理攻撃力UP
-        /// </summary>
-        PhysicalAttackUp,
-        
-        /// <summary>
-        /// 魔法攻撃力UP
-        /// </summary>
-        MagicAttackUp,
-        
-        /// <summary>
-        /// 物理防御力UP
-        /// </summary>
-        PhysicalDefenseUp,
-        
-        /// <summary>
-        /// 魔法防御力UP
-        /// </summary>
-        MagicDefenseUp,
-        
-        /// <summary>
-        /// 回復力UP
-        /// </summary>
-        RecoveryUp,
-        
-        /// <summary>
-        /// 反撃
-        /// </summary>
-        CounterAttack,
-        
-        /// <summary>
-        /// 吸収
-        /// </summary>
-        Absorption,
-    }
-
+    
     public enum ActorType
     {
         Player,
@@ -86,12 +48,7 @@ namespace Cookie
         Epic,
         Legendary,
     }
-
-    public static class EquipmentAttributeName
-    {
-        public const string Strength = "Strength";
-    }
-
+    
     public static class SkillAttributeName
     {
         public const string Power = "Power";
