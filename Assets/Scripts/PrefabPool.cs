4b18a320029e9febdb75b9f84725ad4f326604cc
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-10-28 00:04:16 +0900

    PrefabPool追加

---
+++ Assets/Scripts/PrefabPool.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Pool;
+
+namespace Cookie
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class PrefabPool<T> where T : Component
+    {
+        private ObjectPool<T> objectPool;
+        
+        public PrefabPool(T original, int capacity = 16, int maxSize = 128)
+        {
+            this.objectPool = new ObjectPool<T>(
+                createFunc: () => Object.Instantiate(original),
+                actionOnGet: target => target.gameObject.SetActive(true),
+                actionOnRelease: target => target.gameObject.SetActive(false),
+                actionOnDestroy: target => Object.Destroy(target.gameObject),
+                collectionCheck: true,
+                defaultCapacity: capacity,
+                maxSize: maxSize
+                );
+        }
+
+        public T Get()
+        {
+            return this.objectPool.Get();
+        }
+
+        public void Release(T element)
+        {
+            this.objectPool.Release(element);
+        }
+
+        public void Clear()
+        {
+            this.objectPool.Clear();
+        }
+    }
+}
