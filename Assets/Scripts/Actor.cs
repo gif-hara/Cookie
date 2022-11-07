27a69fa2eea2622fd48a1b65f386707b94b6bfa1
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-11-07 23:40:56 +0900

    相手が死亡していた場合は毒処理しない

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -171,7 +171,7 @@ namespace Cookie
                     }
                     
                     // 毒の処理
-                    if (this.Status.abnormalStatuses.Contains(AbnormalStatus.Poison))
+                    if (this.Status.abnormalStatuses.Contains(AbnormalStatus.Poison) && !x.Opponent.Status.IsDead)
                     {
                         Debug.Log("TODO: 毒演出");
                         this.TakeDamage(Calculator.GetPoisonDamage(this.Status));
